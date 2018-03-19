b713b2d380b72345d7560a8ed81becaaf17704f7
Faradey27 <andriit@wix.com>
2018-03-19 15:54:35 +0200

    Renamed LegendPlugin.js to index.js

--- Contents/Sketch/index.js
+++ Contents/Sketch/index.js
@@ -4,17 +4,17 @@ var Rectangle = require('sketch/dom').Rectangle
 var SYMBOL_INSTANCE_CLASSNAME = "MSSymbolInstance";
 var SYMBOL_MASTER_CLASSNAME = "MSSymbolMaster";
 
-var runLegendScript = function(context) {
+const runLegendScript = function(context) {
   var doc = context.document;
   initSymbolDict(doc)
-  
+
   var pages = doc.pages();
   for (var i = 0; i < pages.length; i++) {
     var currPage = pages[i];
     var artboards = currPage.artboards();
     for (var j = 0; j < artboards.length; j++) {
       var currArtboard = artboards[j];
-      var legendArtboard = createAndAddLegendArtboard(currArtboard, currPage); 
+      var legendArtboard = createAndAddLegendArtboard(currArtboard, currPage);
       legendify(currArtboard, context, currArtboard, legendArtboard, 0);
     }
 
@@ -27,22 +27,22 @@ var initSymbolDict = function(doc) {
 };
 
 var symbolsDict = {};
-var initSymbolDictFlat = function(symbols) {  
+var initSymbolDictFlat = function(symbols) {
   for (var i = 0; i < symbols.count(); i++) {
     var symbol = symbols.objectAtIndex(i);
-    if(symbol.class() == SYMBOL_INSTANCE_CLASSNAME || 
+    if(symbol.class() == SYMBOL_INSTANCE_CLASSNAME ||
        symbol.class() == SYMBOL_MASTER_CLASSNAME) {
       if (symbol.layers) {
         initSymbolDictFlat(symbol.layers());
-      }               
+      }
       symbolsDict[symbol.symbolID()] = symbol;
       symbolsDict[symbol.objectID()] = symbol;
-    } 
+    }
   }
 };
 
 var createAndAddLegendArtboard = function(currArtboard, currPage) {
-  var rect = new Rectangle(currArtboard.frame().x() - 500, currArtboard.frame().y() , 500, 500);  
+  var rect = new Rectangle(currArtboard.frame().x() - 500, currArtboard.frame().y() , 500, 500);
   var artboard = new sketch.Artboard({
     parent: currPage,
     name: 'Legend',
@@ -50,20 +50,20 @@ var createAndAddLegendArtboard = function(currArtboard, currPage) {
     frame: rect,
 
   });
-  artboard.adjustToFit(); 
+  artboard.adjustToFit();
   return artboard;
 };
 
-var legendify = function(comp, context, currArtboard, legendArtboard, prevIndex) { 
+var legendify = function(comp, context, currArtboard, legendArtboard, prevIndex) {
   if (comp.layers) {
     var compLayers = comp.layers();
     var currLegendNextY = 0;
-    for (var k = 0; k < compLayers.length; k++) {     
+    for (var k = 0; k < compLayers.length; k++) {
       var layer = compLayers[k];
       var currentIndex = prevIndex + k;
       if (layer.class() != SYMBOL_INSTANCE_CLASSNAME) {
         legendify(layer, context, currArtboard, legendArtboard, currentIndex);
-      } 
+      }
 
       addIndexesToSymbols(context, layer, currentIndex, currArtboard);
 
@@ -73,24 +73,24 @@ var legendify = function(comp, context, currArtboard, legendArtboard, prevIndex)
         currLegendNextY+=80;
       }
     }
-  } 
-}; 
+  }
+};
 
 var buildOverrideDescription = function(layer, index) {
-  var description ='('+(index)+') '+layer.name()+'\n';       
+  var description ='('+(index)+') '+layer.name()+'\n';
   var overrides = layer.overrides();
-  for (key in overrides) { 
-    var override = overrides[key];          
+  for (key in overrides) {
+    var override = overrides[key];
     if (symbolsDict[key]) {
       description+='        ' + symbolsDict[key].name();
       if (override.symbolID) {
-        var valueString = symbolsDict[override.symbolID]; 
+        var valueString = symbolsDict[override.symbolID];
         description+=" = ";
         description+=valueString.name()+'\n';
       }
-    }            
+    }
   }
-  return description;  
+  return description;
 };
 
 var addIndexesToSymbols = function(context, layer, index, currArtboard) {
@@ -115,5 +115,9 @@ var addDescriptionToLegend = function(str, artboard, nextY) {
     frame: rect,
 
   });
-  text.adjustToFit(); 
+  text.adjustToFit();
 };
+
+
+/* Export plugin command handler */
+this.runLegendScript = runLegendScript;
