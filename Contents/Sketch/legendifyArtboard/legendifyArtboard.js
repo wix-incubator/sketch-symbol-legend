14186ab36850117e074b519d6ed682b9bf8c6f63
Faradey27 <andriit@wix.com>
2018-03-22 15:50:17 +0200

    Fixed wrong x position of legend artboard

--- Contents/Sketch/legendifyArtboard/legendifyArtboard.js
+++ Contents/Sketch/legendifyArtboard/legendifyArtboard.js
@@ -76,13 +76,14 @@ function adjustToFitLegendArtboard(artboard) {
   }
 
   artboard.adjustToFit();
-  artboardFrame.x = artboardFrame.x() - artboardFrame.width();
   artboardFrame.width = artboardFrame.width() + LEGEND_PADDING * 2;
   artboardFrame.height = artboardFrame.height() + LEGEND_PADDING * 2;
   artboardObject.layers().forEach(layer => {
     layer.frame().x = layer.frame().x() + LEGEND_PADDING;
     layer.frame().y = layer.frame().y() + LEGEND_PADDING;
   });
+  // update x position after artboard size and paddings updated
+  artboardFrame.x = artboardFrame.x() - artboardFrame.width();
 }
 
 function legendifyArtboard({ artboard, page, symbolsDictionary }) {
