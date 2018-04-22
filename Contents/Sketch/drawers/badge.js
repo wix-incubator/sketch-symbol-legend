8cc6930b1b8f0bafeb9cd7196bbc17c760873024
Jonathan Bachman <jonathanb@wix.com>
2018-04-22 15:55:15 +0300

    fix(badge): place shape + text in group & position correctly
    Signed-off-by: Jonathan Bachman <jonathanb@wix.com>

--- Contents/Sketch/drawers/badge.js
+++ Contents/Sketch/drawers/badge.js
@@ -1,5 +1,5 @@
 const sketch = require('sketch');
-const { Rectangle, Shape, Style, Text } = require('sketch/dom');
+const { Group, Rectangle, Shape, Text } = require('sketch/dom');
 const setTextColor = require('../utils/setTextColor');
 
 const WIDTH_PER_DIGIT = 15;
@@ -8,21 +8,21 @@ const BADGE_COLOR = '#ff6666';
 const BORDER_COLOR = '#ffffff00';
 const getDigitsCount = num => num.toString().length;
 
-module.exports = (x, y, layerIndex) => {
-  const dimensions = [ getDigitsCount(layerIndex) * WIDTH_PER_DIGIT, WIDTH_PER_DIGIT ];
-
-  const badgeNode = new Shape({
-    frame: new Rectangle(x / 2, y / 2, ...dimensions),
+const createBadgeNode = frame => {
+  return new Shape({
+    frame,
     style: {
       fills: [BADGE_COLOR],
       borders: [BORDER_COLOR]
     }
   });
+}
 
+const createTextNode = (frame, layerIndex) => {
   const textNode = new Text({
     alignment: Text.Alignment.center,
     text: layerIndex.toString(),
-    frame: new Rectangle(x, y, ...dimensions)
+    frame,
   });
 
   setTextColor({
@@ -35,5 +35,21 @@ module.exports = (x, y, layerIndex) => {
 
   textNode.adjustToFit();
 
-  return [badgeNode, textNode];
+  return textNode;
+}
+
+module.exports = (x, y, layerIndex) => {
+  const dimensions = [ getDigitsCount(layerIndex) * WIDTH_PER_DIGIT, WIDTH_PER_DIGIT ];
+  const frame = new Rectangle(0, 0, ...dimensions);
+
+  const groupNode = new Group({
+    name: `Badge ${layerIndex}`,
+    frame: new Rectangle(x, y, ...dimensions),
+    layers: [
+      createBadgeNode(frame),
+      createTextNode(frame, layerIndex)
+    ]
+  });
+
+  return [groupNode];
 };
