1eb6ce93be5c71f2a2977d5144cbabbc0d713c33
Jonathan Bachman <jonathanb@wix.com>
2018-04-22 15:22:27 +0300

    ui(badge): removed border + white text

--- Contents/Sketch/drawers/badge.js
+++ Contents/Sketch/drawers/badge.js
@@ -1,9 +1,11 @@
 const sketch = require('sketch');
-const { Rectangle, Shape } = require('sketch/dom');
+const { Rectangle, Shape, Style, Text } = require('sketch/dom');
+const setTextColor = require('../utils/setTextColor');
 
 const WIDTH_PER_DIGIT = 15;
 const BADGE_COLOR_OPACITY = 40;
 const BADGE_COLOR = '#ff6666';
+const BORDER_COLOR = '#ffffff00';
 const getDigitsCount = num => num.toString().length;
 
 module.exports = (x, y, layerIndex) => {
@@ -12,14 +14,23 @@ module.exports = (x, y, layerIndex) => {
   const badgeNode = new Shape({
     frame: new Rectangle(x / 2, y / 2, ...dimensions),
     style: {
-      fills: [BADGE_COLOR]
+      fills: [BADGE_COLOR],
+      borders: [BORDER_COLOR]
     }
   });
 
-  const textNode = new sketch.Text({
-    alignment: sketch.Text.Alignment.center,
+  const textNode = new Text({
+    alignment: Text.Alignment.center,
     text: layerIndex.toString(),
     frame: new Rectangle(x, y, ...dimensions)
+  });
+
+  setTextColor({
+    textNode,
+    red: 1,
+    green: 1,
+    blue: 1,
+    alpha: 1,
   });
 
   textNode.adjustToFit();
