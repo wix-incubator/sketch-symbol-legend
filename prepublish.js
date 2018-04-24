9bb61fd5d1234d49febdc0a3a3489b3b5c3a7681
Jonathan Bachman <jonathanb@wix.com>
2018-04-24 10:19:30 +0300

    prepublish script updates version also in manifest.json

--- prepublish.js
+++ prepublish.js
@@ -1,15 +1,28 @@
 const { exec } = require('@skpm/internal-utils/exec'); // eslint-disable-line
 const fs = require('fs');
-const fileName = 'README.md';
+const README = 'README.md';
+const MANIFEST = 'Legend.sketchplugin/Contents/Sketch/manifest.json';
 const version = require('./package.json').version;
 
-const updateReadmeVersion = async () => {
-  const fileString = fs.readFileSync(fileName, 'utf8');
+const updateReadmeVersion = () => {
+  const fileString = fs.readFileSync(README, 'utf8');
   const result = fileString.replace(/download\/v[0-9.]*\/Sketch\.zip/, `download/v${version}/Sketch.zip`);
-  fs.writeFileSync(fileName, result, 'utf8');
-  await exec(`git add ${fileName}`);
-  await exec(`git commit -m "Updated readme download link to version ${version}"`);
+  fs.writeFileSync(README, result, 'utf8');
+}
+
+const updateManifestVersion = () => {
+  const manifest = JSON.parse(fs.readFileSync(MANIFEST, 'utf8'));
+  manifest.version = version;
+  fs.writeFileSync(MANIFEST, JSON.stringify(manifest, null, 2), 'utf8');
+}
+
+const gitCommit = async files => {
+  await exec(`git add ${files.join(' ')}`);
+  await exec(`git commit -m "Updated manifest & readme download link to version ${version}"`);
   await exec('git push origin HEAD');
 }
 
+
 updateReadmeVersion();
+updateManifestVersion();
+gitCommit([README, MANIFEST]);
