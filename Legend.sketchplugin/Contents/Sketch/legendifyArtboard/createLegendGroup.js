8c99c4f113c940f4b78084e7bbca7df817eac2de
Jonathan Bachman <jonathanb@wix.com>
2018-04-24 14:10:19 +0300

    added white background to legend content group

--- Legend.sketchplugin/Contents/Sketch/legendifyArtboard/createLegendGroup.js
+++ Legend.sketchplugin/Contents/Sketch/legendifyArtboard/createLegendGroup.js
@@ -1,11 +1,21 @@
-const { Group, Text } = require('sketch/dom'); // eslint-disable-line node/no-missing-require
+const { Group, Text, Shape, } = require('sketch/dom'); // eslint-disable-line node/no-missing-require
 const adjustLegendFrames = require('./adjustLegendFrames');
 const { LEGEND_CONTENT_NAME } = require('../constants');
+
+const LEGEND_GROUP_BACKGROUND = '#ffffff';
 
 const createLegendGroup = ({ artboard, legendItems }) => {
   const legendGroup = new Group({
     parent: artboard,
     name: String(LEGEND_CONTENT_NAME)
+  });
+
+  const legendGroupBackground = new Shape({
+    name: 'Background',
+    parent: legendGroup,
+    style: {
+      fills: [LEGEND_GROUP_BACKGROUND],
+    },
   });
 
   const legendGroupItems = new Text({
@@ -14,7 +24,7 @@ const createLegendGroup = ({ artboard, legendItems }) => {
     fixedWidth: false,
   });
 
-  adjustLegendFrames(artboard, legendGroup, legendGroupItems);
+  adjustLegendFrames(artboard, legendGroup, legendGroupBackground, legendGroupItems);
 
   return legendGroup;
 }
