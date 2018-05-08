4083f3fb55e7a7e374d2fb348a3aa67da410e10b
Chen Almagor <chenalmagor@gmail.com>
2018-05-08 12:55:59 +0300

    Chore: start general solution for text layers

--- Legend.sketchplugin/Contents/Sketch/legendifyArtboard/getLegendItemDescription.js
+++ Legend.sketchplugin/Contents/Sketch/legendifyArtboard/getLegendItemDescription.js
@@ -6,29 +6,32 @@ const getSymbolKey = override =>
 
   const getSymbolDescription = (symbolKey, override, symbolsDictionary) => {
     const symbolID = override && override.symbolID;
-    if (!symbolID) return;
+    // if (!symbolID) return;
 
     const attribute = symbolsDictionary[symbolKey];
     if (!attribute) return;
-
-    const value = symbolsDictionary[symbolID];
+    log('************ ATTRIBUTE');
+    log(attribute.name());
+    const value = symbolsDictionary[symbolID] || attribute;
+    log(value)
+    log(value.name() === attribute.name());
     if (!value) return;
 
     return {
       type: attribute.name(),
-      value: value.name()
+      value: value.name() === attribute.name() ?override :  value.name()
     };
   };
 
-const getSymbolText = (override) => {
-  const textKey = Object.keys(override).find(x => x.match(/^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i));
-  if (override[textKey]) {
-    return {
-      type: "text",
-      value:override[textKey]
-    }
-  }
-}
+// const getSymbolText = (override) => {
+//   const textKey = Object.keys(override).find(x => x.match(/^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i));
+//   if (override[textKey]) {
+//     return {
+//       type: "text",
+//       value:override[textKey]
+//     }
+//   }
+// }
 
 const getOverrideSymbols = (override, symbolKey, symbolsDictionary) => {
   const symbolDescriptions = [];
@@ -36,11 +39,13 @@ const getOverrideSymbols = (override, symbolKey, symbolsDictionary) => {
   let currentKey = symbolKey;
   while (currentOverride) {
     if (currentKey) {
+      log('CURRENT KEY');
+      log(currentKey);
       symbolDescriptions.push(getSymbolDescription(currentKey, currentOverride, symbolsDictionary));
-      const text = getSymbolText(currentOverride)
-      if (text){
-        symbolDescriptions.push(text);
-      }
+      // const text = getSymbolText(currentOverride)
+      // if (text){
+      //   symbolDescriptions.push(text);
+      // }
     }
     currentKey = getSymbolKey(currentOverride);
     currentOverride = currentOverride[currentKey];
@@ -72,6 +77,8 @@ const getOverridesValues = (symbolMaster, symbolsDictionary, overrides) => {
   log({defaultOverrides})
   const finalOverrides = Object.keys(defaultOverrides).map(defaultOverrideKey => {
     const override = overrides[defaultOverrideKey];
+    log({override });
+    log(symbolsDictionary["8EDC973E-36F5-4E4D-AFCB-90549B6BC4E6"]);
     if (override && symbolsDictionary[defaultOverrideKey]) {
       const overrideSymbols = getOverrideSymbols(override, defaultOverrideKey, symbolsDictionary);
       return [defaultOverrides[defaultOverrideKey], ...overrideSymbols];
@@ -94,6 +101,8 @@ log({finalOverrides})
 const getLegendItemDescription = ({ layer, layerIndex, symbolsDictionary }) => {
   const symbolMaster = layer.symbolMaster && layer.symbolMaster();
   const overrides = layer.overrides();
+  log('************* OVERRIDES');
+  log(overrides);
   const componentName = symbolMaster.name().split('/')[1].trim();
   const overridedValues = getOverridesValues(symbolMaster, symbolsDictionary, overrides)
   const data = getComponentData(componentName, {symbolMaster, symbolsDictionary, overridedValues});
