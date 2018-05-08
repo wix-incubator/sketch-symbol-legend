e764c3f3b7450814f8b89f85111cfb92d2c10317
dorr <dor.raba@gmail.com>
2018-05-08 15:36:16 +0300

    fix badge

--- Legend.sketchplugin/Contents/Sketch/legendifyArtboard/adapters/badge.js
+++ Legend.sketchplugin/Contents/Sketch/legendifyArtboard/adapters/badge.js
@@ -2,9 +2,10 @@ const getBadgeProps = (symbolData) => {
   const {symbolMaster, overridedValues} = symbolData;
   const symbolMasterArray = symbolMaster.name().split('/');
   const symbolMasterData = symbolMasterArray[2].split('+');
-  const type =symbolMasterData[1] && symbolMasterData[1].trim();
+  const type =symbolMasterData[1] && symbolMasterData[1].trim() || "solid";
   const prefixIcon =symbolMasterData[2] && symbolMasterData[2].trim() === "Icon Prefix";
   const iconKey = Object.keys(overridedValues).find(x => x.indexOf('Icon') > -1)
+  log({symbolMasterData: symbolMaster.name()})
   return {
     type,
     skin: overridedValues.Type.split('/')[4].trim(),
