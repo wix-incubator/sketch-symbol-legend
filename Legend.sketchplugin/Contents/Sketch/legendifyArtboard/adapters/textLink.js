2814c2429c4ff5b11f9ebf32bea3f8b243a72a25
dorr <dor.raba@gmail.com>
2018-05-09 13:22:19 +0300

    fix(textlink): add more props to adapter

--- Legend.sketchplugin/Contents/Sketch/legendifyArtboard/adapters/textLink.js
+++ Legend.sketchplugin/Contents/Sketch/legendifyArtboard/adapters/textLink.js
@@ -3,10 +3,21 @@ const getTextLinkProps = (symbolData) => {
   const symbolMasterArray = symbolMaster.name().split('/');
   const symbolMasterData = symbolMasterArray[2].split('+');
   const size =symbolMasterData[1].trim();
-  return {
+  const affixType = overridedValues.Affix && overridedValues.Affix.split('/')[4];
+  const iconKey = Object.keys(overridedValues).find(x => x.indexOf('Icon') > -1);
+  log({overridedValues})
+  const props = {
     size,
+    type: overridedValues.Type && overridedValues.Type.split('/')[3],
+    focus: overridedValues.Focus && overridedValues.Focus.split('/')[4],
     text: overridedValues.Value || "Text Link",
+    underlineStyle: (overridedValues.Underline && overridedValues.Underline.split('/')[4].trim() === 'Underline') ? 'always' : 'never',
   }
+
+  if (affixType) {
+    props[affixType.trim().toLowerCase()] = iconKey && overridedValues[iconKey].split('/')[3].trim();
+  }
+  return props
 };
 
 module.exports = {
